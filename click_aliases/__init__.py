73611e18dac581397c53bcfdc1d39eec3d00035b
Tarik Zegmott <tzegmott@gmail.com>
2023-05-05 14:15:14 -0400

    feat: unique partial cmdc
    if a cmd is unique it can be contracted, else error with the matching cmds displayed

--- click_aliases/__init__.py
+++ click_aliases/__init__.py
@@ -58,12 +58,36 @@ class ClickAliasedGroup(click.Group):
         if cmd_name in self._aliases:
             return self._aliases[cmd_name]
         return cmd_name
-
+    
     def get_command(self, ctx, cmd_name):
+        # Step one: bulitin commands as normal
+        rv = click.Group.get_command(self, ctx, cmd_name)
+        if rv is not None:
+            return rv
+
+        # Step two: look up an explicit command alias
         cmd_name = self.resolve_alias(cmd_name)
-        command = super(ClickAliasedGroup, self).get_command(ctx, cmd_name)
+        command = super(AliasedGroup, self).get_command(ctx, cmd_name)
         if command:
             return command
+
+        # Alternative option: if we did not find an explicit alias we
+        # allow automatic abbreviation of the command.  "status" for
+        # instance will match "st".  We only allow that however if
+        # there is only one command.
+        matches = [
+            x for x in self.list_commands(ctx) if x.lower().startswith(cmd_name.lower())
+        ]
+        if not matches:
+            return None
+        elif len(matches) == 1:
+            return click.Group.get_command(self, ctx, matches[0])
+        ctx.fail(f"Too many matches: {', '.join(sorted(matches))}")
+
+    def resolve_command(self, ctx, args):
+        # always return the command's name, not the alias
+        _, cmd, args = super().resolve_command(ctx, args)
+        return cmd.name, cmd, args
 
     def format_commands(self, ctx, formatter):
         rows = []
